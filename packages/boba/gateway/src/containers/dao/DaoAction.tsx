4f5f48113f2a48ab48317a7cc2646c6354defd26
Sahil Kashetwar <sahil@enya.ai>
2023-05-27 16:51:27 +0530

    dao container

---
+++ packages/boba/gateway/src/containers/dao/DaoAction.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { DaoActionContainer } from './style'
+
+interface DaoActionProps {}
+
+export const DaoAction: React.FC<DaoActionProps> = (props: DaoActionProps) => {
+  return <DaoActionContainer></DaoActionContainer>
+}
---
+++ packages/boba/gateway/src/containers/dao/style.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const DaoActionContainer = styled.div`
+  width: 30%;
+  max-width: 250px;
+  display: flex;
+  flexdirection: column;
+  gap: 10px;
+  borderradius: 12px;
+`
